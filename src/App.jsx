2e313aae54a1e9c75f87c8ab4fba142b58f7c8bf
doolotkeldi <dolotalanov35@gmail.com>
2024-05-02 23:38:45 +0600

    the header is over

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage/HomePage";
+import "./css/base/_all.css"
+
+
 function App() {
-  return <div>Hello World</div>;
+  return <>
+  <Routes>
+    
+      <Route path="/" element={<Layout/>}>
+      <Route index element={<HomePage/>}/>
+    </Route>
+  </Routes>
+  </>;
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/svg/Logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header>
+      <div className="container header-content">
+        <Link to="/" className="logo">
+          <img src={Logo} alt="" />
+        </Link>
+        <nav className="header-right">
+          <ul>
+            <li>
+              <Link>
+              Главная
+              </Link>
+              </li>
+            <li>Курсы</li>
+            <li>
+              <button>Преподавател</button>
+            </li>
+            <li>О нас</li>
+            <li>Отзывы</li>
+            <li>Контакт</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
